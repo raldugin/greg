965c21cb814132236e8522e0ce5c1f3af052b2df
Alex Raldugin <a.raldugin@gmail.com>
2017-09-15 07:38:00 +0300

    start session

---
+++ session_and_cookies/content.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: IO
+     * Date: 15.09.2017
+     * Time: 07:34
+     */
+    error_reporting(E_ALL);
---
+++ session_and_cookies/index.php
@@ -0,0 +1,27 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: IO
+     * Date: 15.09.2017
+     * Time: 07:26
+     */
+    error_reporting(E_ALL);
+
+    ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <p><input type="text" name="login"></p>
+    <p><input type="text" name="passworg"></p>
+    <p><input type="submit" name="submit"></p>
+</form>
+</body>
+</html>
